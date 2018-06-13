503d9903145cc92c39dc11a66dbc1980be26e822
IlonaLukuts <40250049+IlonaLukuts@users.noreply.github.com>
2018-06-14 01:43:09 +0300

    Add files via upload

---
+++ Service/ServiceAdmin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessObjects;
+using DataObjects;
+
+namespace Service
+{
+    public class ServiceAdmin
+    {
+        static AdminDao adminDao = new AdminDao();
+        static RequestDao requestDao = new RequestDao();
+        static CardDao cardDao = new CardDao();
+
+        //Service Admin
+        public Admin CheckPassword(string name, string password)
+        {
+            Admin admin = adminDao.GetAdmin(name);
+            if (admin == null)
+                return null;
+            if (admin.Password != password)
+                return null;
+            return admin;
+        }
+
+        //Service Request
+        public List<Request> GetRequests()
+        {
+            return requestDao.GetRequests();
+        }
+
+        public void LeaveBlocked(int requestId)
+        {
+            requestDao.RemoveRequest(requestId);
+        }
+
+        public void UnBlocked(int requestId,int cardId)
+        {
+            cardDao.ChangeBlocked(cardId, false);
+            requestDao.RemoveRequest(requestId);
+        }
+
+    }
+}
---
+++ Service/ServiceUser.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessObjects;
+using DataObjects;
+
+namespace Service
+{
+    public class ServiceUser
+    {
+        static UserDao userDao = new UserDao();
+        static CardDao cardDao = new CardDao();
+        static PaymentDao paymentDao = new PaymentDao();
+        static RequestDao requestDao = new RequestDao();
+
+        //Service User
+
+        public User CheckPassword(string name, string password)
+        {
+            User user = userDao.GetUser(name);
+            if (user == null)
+                return null;
+            if (user.Password != password)
+                return null;
+            cardDao.UpdateActive();
+            paymentDao.UpdateOldPayments();
+            return user;
+        }
+        public User AddUser(string name,string password)
+        {
+            if (userDao.GetUser(name) != null)
+                return null;
+            else
+            {
+                User user = new User();
+                user.Name = name;
+                user.Password = password;
+                userDao.AddUser(user);
+                return user;
+            }
+        }
+        //Service Card
+        public List<Card> GetCards(int userId)
+        {
+            return cardDao.GetCards(userId);
+        }
+
+        public Card NewCard(string numberCard,int userId)
+        {
+            return cardDao.AddUser(numberCard, userId);
+        }
+
+        public void BlockedCard(int id)
+        {
+            cardDao.ChangeBlocked(id, true);
+        }
+        public bool UnBlockedCard(int id,int cvv,string numberCard)
+        {
+            if(cardDao.CheckCVV(id,cvv))
+            {
+                Request request = new Request();
+                request.CardId = id;
+                request.NumberCard = numberCard;
+                requestDao.AddRequest(request);
+                return true;
+            }
+            return false;
+        }
+
+        //Service Payment
+        public List<Payment> GetPayments(int id)
+        {
+            return paymentDao.GetPayments(id);
+        }
+
+        public bool NewPayment(int cardId, string account, decimal money, string notice)
+        {
+            if (!cardDao.CheckEnoughMoney(cardId, money))
+                return false;
+            else
+            {
+                Payment payment = new Payment();
+                payment.CardId = cardId;
+                payment.Account = account;
+                payment.Date = DateTime.Now;
+                payment.Money = money;
+                payment.Notice = notice;
+                paymentDao.AddPayment(payment);
+                cardDao.UpdateBalance(cardId, money*-1);
+                cardDao.UpdateBalance(account, money);
+                return true;
+            }
+        }
+    }
+}
