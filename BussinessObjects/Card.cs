b8c21d723b671d8dea358221aa591e2313e4a7ac
IlonaLukuts <40250049+IlonaLukuts@users.noreply.github.com>
2018-06-14 01:13:36 +0300

    Add files via upload

---
+++ BussinessObjects/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string NumberAccount { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Validity { get; set; }
+        public int CVV { get; set; }
+        public int? UserId { get; set; }
+    }
+}
---
+++ BussinessObjects/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public int CardId { get; set; }
+        public string NumberCard { get; set; }
+
+    }
+}
